e2da8ba0475a4fbe635a7424cdccf1920d733962
Ulcor <balgalvalex@gmail.com>
2025-08-23 00:40:47 +0300

    [wexler]: Slight folder beautify. Lists start

---
+++ Python/Courses/Wexler io, big Python course/Module 4. Cycles/Lists.py
@@ -0,0 +1,13 @@
+list_1 = list(range(10))
+# ctrl + click to see builtins.py, and all methods in it
+list_1.remove(1)
+list_1.append(2)
+list_1.index(0)
+
+print(list_1)
+
+list_1 = list_1.pop(2)
+
+print(list_1)
+
+# Write add_to_grocery_list function
