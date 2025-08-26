6d1bac3d55d8c08db9332146cf3e1d54a2f52e89
Ulcor <balgalvalex@gmail.com>
2025-08-26 15:01:14 +0300

    [wexler]: Lists. And unfinished bigquery loader

--- Python/Courses/Wexler io, big Python course/Module 4. Cycles/Lists.py
+++ Python/Courses/Wexler io, big Python course/Module 4. Cycles/Lists.py
@@ -11,3 +11,60 @@ list_1 = list_1.pop(2)
 print(list_1)
 
 # Write add_to_grocery_list function
+to_buy = ['avocado', 'milk', 'eggs']
+
+# def add_to_grocery_list(grocery_list: list, new_item: str):
+#     grocery_list.append(new_item)
+
+
+# def remove_last_from_grocery_list(grocery_list: list):
+#     return grocery_list.pop()
+#
+# remove_last_from_grocery_list(to_buy)
+
+# def remove_first_from_grocery_list(grocery_list: list):
+#     return grocery_list.pop(0)
+#
+# remove_first_from_grocery_list(to_buy)
+
+# Delete by index
+# def remove_from_grocery_list(grocery_list: list, remove: str):
+#     try:
+#         grocery_list.pop(grocery_list.index(remove))
+#     except ValueError as error:
+#         print(error)
+#
+# removed = remove_from_grocery_list(to_buy, 'milk')   # milk
+# remove_from_grocery_list(to_buy, 'chicken')  # вызовет ошибку ValueError
+#
+# print(removed)
+# print(to_buy)
+
+# Delete by index but simpler
+# def remove_from_grocery_list(grocery_list: list, item: str):
+#     return grocery_list.pop(grocery_list.index(item))
+#
+# removed = remove_from_grocery_list(to_buy, 'milk')   # milk
+# remove_from_grocery_list(to_buy, 'chicken')  # вызовет ошибку ValueError
+#
+# print(removed)
+# print(to_buy)
+
+# List v2
+workers = ['John', 'Doe', 'Michael']
+
+# def get_first(names: list):
+#     return names[0]
+#
+# print(get_first(workers))
+
+# def get_last(names: list):
+#     return names[-1]
+#
+# print(get_last(workers))
+
+# Inversion
+def get_workers(names: list):
+    return names[::-1]
+
+print(get_workers(workers))
