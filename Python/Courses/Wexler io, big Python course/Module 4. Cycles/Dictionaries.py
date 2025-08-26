4afa11ec268ec8dd557be77f52ccf60b56c36dde
Ulcor <balgalvalex@gmail.com>
2025-08-27 00:33:47 +0300

    [wexler]: everything you can do with Dictionaries

--- Python/Courses/Wexler io, big Python course/Module 4. Cycles/Dictionaries.py
+++ Python/Courses/Wexler io, big Python course/Module 4. Cycles/Dictionaries.py
@@ -5,7 +5,35 @@ def add_to_dictionary(diction: dict, added_word: str, translation_en: str):
     diction[added_word] = translation_en
 
 
+def translate(diction: dict, word: str):
+    # return diction[word]  # this is a key. Will return value
+    # return diction.get(word)  # same but with None handling
+    # if diction.get(word) is None:  # Same but prints exactly what happened in case of None
+    #     return f"Слово {word} не найдено"
+    # else:
+    #     return diction.get(word)
+    return diction.get(word, f"Слово {word} не найдено")  # Same but in 1 row
+
+
 dictionary = {}  # задаём пустую переменную
 add_to_dictionary(dictionary, "Масло", "Butter")  # Вызываем вашу функцию
 
 print(dictionary)  # Будет содержать  {"Масло": "Butter"}
+
+translation = translate(dictionary, "Масло")
+print(translation)   # Будет содержать "Butter"
+
+def get_words(diction: dict):
+    return diction.keys()
+
+print(get_words(dictionary))
+
+def get_translations(diction: dict):
+    return diction.values()
+
+print(get_translations(dictionary))
+
+def get_entries(diction: dict):
+    return diction.items()
+
+print(get_entries(dictionary))
