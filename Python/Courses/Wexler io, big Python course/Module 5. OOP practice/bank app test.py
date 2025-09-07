b7e1ebc11248c490ce008a019decab7e757b9ac5
Ulcor <balgalvalex@gmail.com>
2025-09-07 23:30:21 +0300

    [wexler]: Bank app step 2

--- Python/Courses/Wexler io, big Python course/Module 5. OOP practice/bank app test.py
+++ Python/Courses/Wexler io, big Python course/Module 5. OOP practice/bank app test.py
@@ -4,6 +4,16 @@ class Customer:
         self.last_name = last_name
         self.passport_number = passport_number
 
+
 class Bank:
-    def __init__(self):
-        self.customers = list()
+    def __init__(self): # variables
+        self.customers = list()  # can use self. syntax even with no parameters
+
+    def add_customer(self, first_name: str, last_name: str, passport_number: int):
+        self.customers.append(Customer(first_name, last_name, passport_number))
+
+
+bank = Bank()
+bank.add_customer(first_name="John", last_name="Smith", passport_number=12345)
+
+
