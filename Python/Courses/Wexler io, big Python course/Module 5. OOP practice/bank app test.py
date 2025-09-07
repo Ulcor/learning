f7d59deae4a09426354b240d4fd2a5f34aa7c74d
Ulcor <balgalvalex@gmail.com>
2025-09-07 23:53:20 +0300

    [wexler]: Bank app step 3

--- Python/Courses/Wexler io, big Python course/Module 5. OOP practice/bank app test.py
+++ Python/Courses/Wexler io, big Python course/Module 5. OOP practice/bank app test.py
@@ -4,6 +4,9 @@ class Customer:
         self.last_name = last_name
         self.passport_number = passport_number
 
+    def __repr__(self):  # для удобного вывода
+        return f"Customer({self.first_name} {self.last_name}, {self.passport_number})"
+
 
 class Bank:
     def __init__(self): # variables
@@ -12,8 +15,19 @@ class Bank:
     def add_customer(self, first_name: str, last_name: str, passport_number: int):
         self.customers.append(Customer(first_name, last_name, passport_number))
 
+    def get_customer(self, passport_number: int):
+       for cust in self.customers:
+           # Why it works: [Customer("John", "Smith", 12345)], NOT ["John", "Smith", 12345]
+           if cust.passport_number == passport_number:
+               return cust
+       raise KeyError
+
 
 bank = Bank()
 bank.add_customer(first_name="John", last_name="Smith", passport_number=12345)
+customer = bank.get_customer(12345)
+print(customer)
+# customer = bank.get_customer(2313)
+# print(customer)
 
 
